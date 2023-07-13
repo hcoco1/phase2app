e62815137f5bc5717590a7ffa540211804d549ec
Ivan <arias.ivan@gmail.com>
2023-07-13 04:03:31 -0400

    isActive className added to the nav

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,24 +1,37 @@
 
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 
 function NavigationBar() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "none",
+    color: "var(--success)"
+  }
   return (
 
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" sticky="top" >
       <Container>
         <Navbar.Brand>
-          <Link className="linkNav" to="/">Home</Link>
+          <NavLink className="linkNav" style={({ isActive }) => isActive ? activeStyles : null} end to="/">Home</NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavLink id="RouterNavLink" className="linkNav" to="properties/list">Properties</NavLink>
-            <NavLink id="RouterNavLink" className="linkNav" to="/search">Search</NavLink>
-            <NavLink id="RouterNavLink" className="linkNav" to="charts">Charts</NavLink>
+            <NavLink
+              id="RouterNavLink"
+              className="linkNav"
+              style={({ isActive }) => isActive ? activeStyles : null}
+              to="properties/list"
+            >
+              Properties
+            </NavLink>
+
+            <NavLink id="RouterNavLink" className="linkNav" style={({ isActive }) => isActive ? activeStyles : null} to="/search">Search</NavLink>
+            <NavLink id="RouterNavLink" className="linkNav" style={({ isActive }) => isActive ? activeStyles : null} to="charts">Charts</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>

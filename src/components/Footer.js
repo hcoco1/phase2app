5b86f23e46cacc48fd2131bf7701a41e0336a817
Ivan <arias.ivan@gmail.com>
2023-07-25 21:16:58 -0400

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -91,7 +91,7 @@ export default function FooterHome() {
       </MDBContainer>
 
       <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
+        © 2023 Copyright:
       
       </div>
       </MDBFooter>
